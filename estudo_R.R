bb30f2a87a38a78f201667e2a8a06daf586d8c9a
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2023-02-10 10:30:07 -0300

    analise renda media sexo_cor

---
+++ estudo_R.R
@@ -0,0 +1,35 @@
+dados <- read.csv('estatistica-r-parte-1-arquivos-projeto-inicial/dados.csv')
+
+head(dados)
+
+dist_freq_quali <- cbind(freq = table(dados$Sexo), porcent = prop.table((table(dados$Sexo)) * 100))
+dist_freq_quali
+
+colnames(dist_freq_quali) <- c('frequencia', 'porcentagem')
+
+rownames(dist_freq_quali) <- c('masculino', 'feminino')
+
+dist_freq_quali
+
+frequencia <- table(dados$Sexo, dados$Cor)
+frequencia
+
+rownames(frequencia) <- c('masculino', 'feminino')
+
+colnames(frequencia) <- c('indigena', 'branca', 'preta', 'amarela', 'parda')
+
+frequencia
+
+dist_freq_quali_percent <- prop.table(frequencia) * 100
+
+dist_freq_quali_percent <- round(dist_freq_quali_percent, digits=2)
+
+dist_freq_quali_percent
+
+
+renda_media <- tapply(dados$Renda, list(dados$Sexo, dados$Cor), mean)
+
+rownames(renda_media) <- c('masculino', 'feminino')
+colnames(renda_media) <- c('indigena', 'branca', 'preta', 'amarela', 'parda')
+
+renda_media
