2e394dcf5b4e741631688a2228fc415db74835a2
Victor Pecine <73555379+victorpecine@users.noreply.github.com>
2023-02-10 16:57:59 -0300

    medidas separatrizes

--- estudo_R.R
+++ estudo_R.R
@@ -40,13 +40,13 @@ labels <- c('D', 'C', 'B', 'A')
 
 
 frequencia <- table(
-                    cut(
-                    x = dados$Renda,
-                    breaks = classes,
-                    labels = labels,
-                    include.lowest = TRUE
-                    )
-)
+  cut(
+    x = dados$Renda,
+    breaks = classes,
+    labels = labels,
+    include.lowest = TRUE
+    )
+  )
 
 frequencia
 
@@ -65,20 +65,20 @@ classes_altura <- c(min(dados$Altura), 1.65, 1.75, max(dados$Altura))
 labels_altura <- c('1 - Baixa', '2 - Média', '3 - Alta')
 
 frequencia <- table(
-                    cut(
-                      x = dados$Altura,
-                      breaks = classes_altura,
-                      labels = labels_altura,
-                      include.lowest = TRUE
-                    )
-)
+  cut(
+    x = dados$Altura,
+    breaks = classes_altura,
+    labels = labels_altura,
+    include.lowest = TRUE
+    )
+  )
 
 percentual <- prop.table(frequencia) * 100
 
 dist_freq_altura <- cbind('frequencia' = frequencia, 'porcentagem' = percentual)
 
 dist_freq_altura[
-                 order(row.names(dist_freq_altura)),
+  order(row.names(dist_freq_altura)),
 ]
 
 
@@ -93,12 +93,12 @@ k <- floor(k)
 k
 
 frequencia <- table(
-                    cut(
-                      x = dados$Renda,
-                      breaks = k,
-                      include.lowest = TRUE
-                    )
-)
+  cut(
+    x = dados$Renda,
+    breaks = k,
+    include.lowest = TRUE
+    )
+  )
 
 frequencia <- cbind(frequencia)
 frequencia
@@ -125,3 +125,71 @@ hist(x=dados$Altura,
      ylim=c(0, 20000)
 )
 
+
+install.packages('ggplot2')
+library(ggplot2)
+
+ggplot(data = dados, aes(x = Altura)) +
+  geom_histogram(binwidth = 0.02,
+                 color = 'black',
+                 alpha = 0.3) +
+  ylab('Frequência') +
+  xlab('Altura') +
+  ggtitle('Histograma das alturas') +
+  theme(plot.title = element_text(size = 14, hjust = 0.5),
+        axis.title.y = element_text(size = 12, vjust = +0.2),
+        axis.title.x = element_text(size = 12, vjust = -0.2),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10)
+        )
+
+ggplot(dados, aes(x = Altura, y = after_stat(density))) +
+  geom_histogram(binwidth = 0.02,
+                 color = 'black',
+                 alpha = 0.8,
+                 fill = 'lightgreen') +
+  geom_density(color='black', linewidth = 1) +
+  ylab('Frequência') +
+  xlab('Altura') +
+  ggtitle('Histograma das alturas') +
+  theme(plot.title = element_text(size = 14, hjust = 0.5),
+        axis.title.y = element_text(size = 12, vjust = +0.2),
+        axis.title.x = element_text(size = 12, vjust = -0.2),
+        axis.text.y = element_text(size = 10),
+        axis.text.x = element_text(size = 10)
+  )
+
+
+# QUartis
+quantile(dados$Renda, c(0.25, 0.5, 0.75))
+
+# Decis
+decis <- c()
+for (i in 1:9){
+  decis <- c(decis, i / 10)
+}
+decis
+
+quantile(dados$Renda, decis)
+
+# Centis
+centis <- c()
+for (i in 1:99){
+  centis <- c(centis, i / 100)
+}
+centis
+
+quantile(dados$Renda, centis)
+
+
+# Distribuição de frequência acumulada
+ggplot(data = dados, aes(x = Idade)) +
+  geom_histogram(
+    aes(y = cumsum(..count..) / sum(..count..)),
+    bins = 10,
+  ) +
+  geom_freqpoly(
+    aes(y = cumsum(..count..) / sum (..count..)),
+    color = 'lightblue',
+    linewidth = 1
+  )
